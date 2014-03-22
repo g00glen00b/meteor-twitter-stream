37a7ac72a2013b9feebe351f1d944d45921452d9
g00glen00b <me@g00glen00b.be>
2014-03-22 19:05:01 +0100

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,17 @@
+var Twitter = Meteor.require("twitter");
+var TweetStream = new Meteor.Stream('tweets');
+var conf = JSON.parse(Assets.getText('twitter.json'));
+var twit = new Twitter({
+	consumer_key: conf.consumer.key,
+	consumer_secret: conf.consumer.secret,
+	access_token_key: conf.access_token.key,
+	access_token_secret: conf.access_token.secret
+});
+
+twit.stream('statuses/filter', {
+	'track': conf.hashtag
+}, function(stream) {
+	stream.on('data', function(data) {
+		TweetStream.emit('tweet', data);
+	});
+});
